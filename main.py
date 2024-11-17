4ba1d6c77349028412ce61f225ce0a4a1e4f3ff7
rizashrestha1 <rizashrestha819@gmail.com>
2024-11-17 22:50:13 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import math_utils
+import mathematics.trigonometry
+from mathematics import trigonometry
+import mathematics
+from mathematics.trigonometry import sine
+
+sum = math_utils.add(1,2)
+diff = math_utils.subtract(2,3)
+print(sum,diff)
+pi=3.14
+sin_45 = sine(pi/4)
+print(sin_45)
